5239267b2ce018044d0ecfe18d44aca8991d4776
hyun99999 <ypsophila97@icloud.com>
2022-09-11 03:35:56 +0900

    add: 이중우선순위큐
    - 우선순위 큐를 활용한 풀이

--- Level3/이중우선순위큐.swift
+++ Level3/이중우선순위큐.swift
@@ -1,3 +1,4 @@
+/*
 import Foundation
 
 func solution(_ operations:[String]) -> [Int] {
@@ -25,8 +26,66 @@ func solution(_ operations:[String]) -> [Int] {
     
     return queue.isEmpty ? [0, 0] : [queue.max()!, queue.min()!]
 }
+*/
 
 // 우선순위 큐지만 전혀 큐를 사용하지 않은 풀이였고, 이중우선순위 큐를 사용하여 풀어보자.
+
+import Foundation
+
+func solution(_ operations:[String]) -> [Int] {
+    
+    var priorityQueue: [Int] = []
+    
+    // 우선순위 큐 생성.
+    func makePriorityQueue(_ number : Int){
+        var low: Int = 0
+        var mid: Int = 0
+        var high: Int = priorityQueue.count - 1
+        
+        if priorityQueue.isEmpty {
+            priorityQueue.append(number)
+            return
+        }
+        
+        while low <= high {
+            mid = (low + high) / 2
+            
+            if priorityQueue[mid] < number {
+                low = mid + 1
+            } else {
+                // priorityQueue[mid] == number 일때 low 를 반환.
+                high = mid - 1
+            }
+        }
+        
+        priorityQueue.insert(number, at: low)
+        
+    }
+    
+    for element in operations{
+        let split: [String] = element.components(separatedBy: " ")
+        let commend = split[0]
+        let number = split[1]
+        
+        if commend == "I" {
+            // 삽입.
+            makePriorityQueue(Int(number)!)
+        } else {
+            if !priorityQueue.isEmpty {
+                if number == "1" {
+                    // 최댓값 삭제
+                    priorityQueue.removeLast()
+                } else {
+                    // 최솟값 삭제
+                    priorityQueue.removeFirst()
+                }
+            }
+        }
+    }
+    
+    return priorityQueue.isEmpty ? [0, 0] : [priorityQueue.max()! , priorityQueue.min()!]
+}
+
 
 print(solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]))
 // [0, 0]
