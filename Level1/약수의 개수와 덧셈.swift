9a7dfa3e06e7801080299f46a1c2d44d843065ab
hyun99999 <ypsophila97@icloud.com>
2022-10-20 12:36:41 +0900

    [Add] 월간 코드 챌린지 시즌2 - 약수의 개수와 덧셈
    - 함수로 분리한 코드 추가

--- Level1/약수의 개수와 덧셈.swift
+++ Level1/약수의 개수와 덧셈.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+/*
 func solution(_ left:Int, _ right:Int) -> Int {
     var sum: Int = 0
     for i in left...right {
@@ -21,3 +21,32 @@ func solution(_ left:Int, _ right:Int) -> Int {
     
     return sum
 }
+*/
+
+// 함수로 분리
+
+func solution(_ left:Int, _ right:Int) -> Int {
+    var sum: Int = 0
+    
+    for i in left...right {
+        if divisorNumberIsEven(i) {
+            sum += i
+        } else {
+            sum -= i
+        }
+    }
+    
+    return sum
+}
+
+func divisorNumberIsEven(_ number: Int) -> Bool {
+    var count: Int = 0
+    
+    for n in 1...number {
+        if number % n == 0 {
+            count += 1
+        }
+    }
+    return count % 2 == 0 ? true : false
+}
+
