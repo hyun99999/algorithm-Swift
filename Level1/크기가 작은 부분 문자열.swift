14252401d5196270c3387d132b8435fcc0db2ed1
hyun99999 <ypsophila97@icloud.com>
2023-01-11 00:02:56 +0900

    refactor: 크기가 작은 부분 문자열

--- Level1/크기가 작은 부분 문자열.swift
+++ Level1/크기가 작은 부분 문자열.swift
@@ -1,21 +1,18 @@
 import Foundation
 
 // p 의 길이만큼의 부분문자열 중 p 이하인 횟수 반환.
+
 func solution(_ t:String, _ p:String) -> Int {
     let number: Int = p.count
     let p: Int = Int(p) ?? 0
-    let t: [Int] = t.map { Int(String($0)) ?? 0 }
+    let t: [String] = t.map { String($0) }
     
     var answer: Int = 0
     
     for i in 0...t.count - number {
-        let slicedT: [Int] = Array(t[i..<(i + number)])
-        var joinedT: String = ""
-        slicedT.forEach { number in
-            joinedT.append(String(number))
-        }
+        let slicedT: Int = Int(t[i..<(i + number)].reduce("", +)) ?? 0
         
-        if p >= Int(joinedT) ?? 0 {
+        if p >= slicedT {
             answer += 1
         }
     }
@@ -23,8 +20,9 @@ func solution(_ t:String, _ p:String) -> Int {
     return answer
 }
 
-
 print(solution("3141592", "271"))
 // 2
 print(solution("10203", "15"))
 // 3
+
+
