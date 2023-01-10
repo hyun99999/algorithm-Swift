8e4554743b3a8a77dfca678d965634b05e93301f
hyun99999 <ypsophila97@icloud.com>
2023-01-10 23:38:51 +0900

    feat: 연습문제 - 크기가 작은 부분 문자열

---
+++ Level1/크기가 작은 부분 문자열.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+// p 의 길이만큼의 부분문자열 중 p 이하인 횟수 반환.
+func solution(_ t:String, _ p:String) -> Int {
+    let number: Int = p.count
+    let p: Int = Int(p) ?? 0
+    let t: [Int] = t.map { Int(String($0)) ?? 0 }
+    
+    var answer: Int = 0
+    
+    for i in 0..<t.count - number {
+        let slicedT: [Int] = Array(t[i..<(i + number)])
+        var joinedT: String = ""
+        slicedT.forEach { number in
+            joinedT.append(String(number))
+        }
+        
+        if p >= Int(joinedT) ?? 0 {
+            answer += 1
+        }
+    }
+    
+    return answer
+}
+// 실패. 36.8 / 100.0
+
+
+print(solution("3141592", "271"))
+// 2
