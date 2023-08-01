ff154bc25fea9e08d0b8555bf7a45ff4b5bf74c1
hyun99999 <ypsophila97@icloud.com>
2023-08-01 09:56:40 +0900

    feat: 백준 - LCS(gold5)
    - 풀이방법 추가.

--- Gold/LCS.swift
+++ Gold/LCS.swift
@@ -4,11 +4,12 @@ let a: [String] = readLine()!.map { String($0) }
 let b: [String] = readLine()!.map { String($0) }
 
 // 2차원 dp
-var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: a.count + 1), count: b.count + 1)
+//var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: a.count + 1), count: b.count + 1)
 
-// 아이디어: 같은 알파벳의 경우, 비교 전의 lcs 값보다 1 더한다.
+// ✅아이디어: 같은 알파벳의 경우, 비교 전의 lcs 값보다 1 더한다.
 // 출처: https://myjamong.tistory.com/317
 
+/*
 for row in 1..<dp.count {
     for col in 1..<dp[0].count {
         if b[row - 1] == a[col - 1] {
@@ -21,10 +22,24 @@ for row in 1..<dp.count {
 }
 
 print(dp[b.count][a.count])
+*/
 
-/*
- ACAYKP
- CAPCAK
- */
+// ✅아이디어: 2차원 dp 대신 1차원 dp 사용 가능.
+// 2차원 배열을 순회하지 않아도 되서 메모리와 시간복잡도가 작다.
 
-// 4
+var dp: [Int] = Array(repeating: 0, count: a.count)
+
+for i in 0..<b.count {
+    var count: Int = 0
+    
+    for j in 0..<a.count {
+        if count < dp[j] {
+            // 동일한 글자 여부 확인보다 먼저, 누적된 값이 dp 값보다 작은지 확인.
+            count = dp[j]
+        } else if b[i] == a[j] {
+            dp[j] = count + 1
+        }
+    }
+}
+
+print(dp.max()!)
